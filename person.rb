54ab75a260cf2158839e04f6e102b77e880daaa5
Dachrono <andres_zamorano_@hotmail.com>
2023-08-29 20:32:38 -0600

    Create person class and the requirements

---
+++ person.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# Person class
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(age, name: 'Unknown', parent_permision: true)
+    @id = id
+    @name = name
+    @age = age
+    @parent_permision = parent_permision
+  end
+
+  def can_use_services?
+    of_age? || @parent_permision
+  end
+
+  private
+
+  def of_age?
+    age >= 18
+  end
+end
