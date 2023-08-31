4f385cd8dbe0d9fa674cd00dff47a1448334fdb1
Dachrono <andres_zamorano_@hotmail.com>
2023-08-31 16:12:52 -0600

    Create file decorator and the requirements

---
+++ decorator.rb
@@ -0,0 +1,12 @@
+require_relative 'nameable'
+# decorator class
+class Decorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
