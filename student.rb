421c0ff390d0314ca10bc07879301b58a8e33c5e
Dachrono <andres_zamorano_@hotmail.com>
2023-09-01 14:01:37 -0600

    Modify student class and classroom to create requirement relationship

--- student.rb
+++ student.rb
@@ -2,12 +2,20 @@ require_relative 'person'
 
 # Student class
 class Student < Person
-  def initialize(age, classroom, name: 'Unknown', parent_permision: true)
-    super(age, name, parent_permision)
+  attr_reader :classroom
+
+  def initialize(age, classroom, name = 'Unknown', parent_permision: true)
+    super(age, name, parent_permision: parent_permision)
     @classroom = classroom
   end
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless
+    classroom.students.include?(self)
   end
 end
