0f27faf1daba76cd5afdfc004b497f7b188a11d8
Dachrono <andres_zamorano_@hotmail.com>
2023-09-01 13:58:10 -0600

    Create file classroom and the requirements

---
+++ classroom.rb
@@ -0,0 +1,15 @@
+# classroom class
+class Classroom
+  attr_accessor :label
+  attr_reader :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students << student
+    student.classroom = self
+  end
+end
